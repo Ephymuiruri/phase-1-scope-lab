1690419dc5398c9fdfea64997da9c949a0c71ec8
Ephymuiruri <161690329+Ephymuiruri@users.noreply.github.com>
2024-03-25 18:47:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+// Write your solution in this file!//
+var customerName='bob'
+function upperCaseCustomerName(){
+customerName=customerName.toUpperCase()
+}
+function setBestCustomer (){
+     bestCustomer= 'not bob' 
+}
+setBestCustomer()
+console.log(bestCustomer)
+function overwriteBestCustomer(){
+    bestCustomer= 'maybe bob'
+   return bestCustomer
+}
+overwriteBestCustomer()
+console.log(bestCustomer)
+const leastFavoriteCustomer = "Elva"
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "Joy"
+}
+
